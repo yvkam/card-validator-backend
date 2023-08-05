b61d8b8ed9b10a21e6d2d78ffce8f657b51e4986
yvkam <kambalayves@gmail.om>
2023-08-05 17:18:48 +0200

    feat: database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -7,8 +7,16 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
       password: env('DATABASE_PASSWORD'),
-      ssl: {
-        rejectUnauthorized:env.bool('DATABASE_SSL_SELF', false),
+      ssl: env.bool('DATABASE_SSL', false) && {
+        key: env('DATABASE_SSL_KEY', undefined),
+        cert: env('DATABASE_SSL_CERT', undefined),
+        ca: env('DATABASE_SSL_CA', undefined),
+        capath: env('DATABASE_SSL_CAPATH', undefined),
+        cipher: env('DATABASE_SSL_CIPHER', undefined),
+        rejectUnauthorized: env.bool(
+          'DATABASE_SSL_REJECT_UNAUTHORIZED',
+          true
+        ),
       },
     },
     debug: false,
